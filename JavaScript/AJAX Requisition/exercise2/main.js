23fc7fc1509628852efcb11d14c7eb2cf660fb31
jonjon667 <jonjon667@hotmail.com>
2020-03-14 02:50:15 -0300

    Updating with AJAX Request exercise 2

---
+++ JavaScript/AJAX Requisition/exercise2/main.js
@@ -0,0 +1,20 @@
+var input = document.querySelector('input[name=user]')
+var container = document.querySelector('div[id=app]')
+var btnSearch = document.querySelector('button[id=btnSearch')
+var itemList = document.createElement('ul')
+container.appendChild(itemList)
+btnSearch.onclick = function (){
+    var endereco =  'https://api.github.com/users/'+ input.value +'/repos'
+    axios.get(endereco)
+        .then(function(response){
+            console.log(response.data)
+            itemList.innerHTML = ''
+            for (repository in response.data) {
+                listItem = document.createElement('li')
+                console.log(repository)
+                var repositoryName = document.createTextNode(response.data[repository].name)
+                listItem.appendChild(repositoryName)
+                itemList.appendChild(listItem)
+            }
+        })
+    }
